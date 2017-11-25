6d4db0e32f215ff63f2a944992c43ad61320ede6
Payam Naderi <naderi.payam@gmail.com>
2017-11-25 23:05:08 +0330

    Implement my profile include extra information

--- src/Actions/GetMyProfileAction.php
+++ src/Actions/GetMyProfileAction.php
@@ -1,9 +1,13 @@
 <?php
 namespace Module\Profile\Actions;
 
+use Module\Content\Interfaces\Model\Repo\iRepoPosts;
 use Module\HttpFoundation\Actions\Url;
 use Module\HttpFoundation\Events\Listener\ListenerDispatch;
+use Module\Profile\Events\EventsHeapOfProfile;
+use Module\Profile\Interfaces\Model\Repo\iRepoFollows;
 use Module\Profile\Interfaces\Model\Repo\iRepoProfiles;
+use Module\Profile\Model\Entity\EntityFollow;
 use Module\Profile\Model\Entity\EntityProfile;
 use Poirot\Http\Interfaces\iHttpRequest;
 use Poirot\OAuth2Client\Interfaces\iAccessToken;
@@ -19,6 +23,10 @@ class GetMyProfileAction
 
     /** @var iRepoProfiles */
     protected $repoProfiles;
+    /** @var iRepoFollows */
+    protected $repoFollows;
+    /** @var iRepoPosts */
+    protected $repoPosts;
 
 
     /**
@@ -26,12 +34,21 @@ class GetMyProfileAction
      *
      * @param iHttpRequest $httpRequest   @IoC /HttpRequest
      * @param iRepoProfiles $repoProfiles @IoC /module/profile/services/repository/Profiles
+     * @param iRepoFollows  $repoFollows  @IoC /module/profile/services/repository/Follows
+     * @param iRepoPosts    $repoPosts    @IoC /module/content/services/repository/Posts
      */
-    function __construct(iHttpRequest $httpRequest, iRepoProfiles $repoProfiles)
+    function __construct(
+        iHttpRequest $httpRequest
+        , iRepoProfiles $repoProfiles
+        , iRepoFollows $repoFollows
+        , iRepoPosts $repoPosts
+    )
     {
         parent::__construct($httpRequest);
 
         $this->repoProfiles = $repoProfiles;
+        $this->repoFollows  = $repoFollows;
+        $this->repoPosts    = $repoPosts;
     }
 
 
@@ -66,6 +83,14 @@ class GetMyProfileAction
         $entity = $this->repoProfiles->findOneByUID( $userid );
 
 
+        # Count Statistics
+        #
+        // TODO Some data must inject with events attached to this action
+        $cntFollowers  = $this->repoFollows->getCountAllForIncoming($userid, [EntityFollow::STAT_ACCEPTED]);
+        $cntFollowings = $this->repoFollows->getCountAllForOutgoing($userid, [EntityFollow::STAT_ACCEPTED]);
+        $cntPosts      = $this->repoPosts->getCountMatchWithOwnerId($userid);
+
+
         # Build Response
         #
         $r = [
@@ -78,7 +103,13 @@ class GetMyProfileAction
                 , [ 'userid' => $oauthInfo['user']['uid'] ]
                 , Url::ABSOLUTE_URL | Url::DEFAULT_INSTRUCT
             ),
-            'personal' => ($entity) ? [
+
+            'followers_count'  => $cntFollowers,
+            'followings_count' => $cntFollowings,
+            'posts_count'      => $cntPosts,
+            'score' => 0, // TODO
+
+            'profile' => ($entity) ? [
                 'bio'      => (string) $entity->getBio(),
                 'gender'   => (string) $entity->getGender(),
                 'location'   => ($entity->getLocation()) ? [
@@ -101,6 +132,20 @@ class GetMyProfileAction
             'is_valid' => $oauthInfo['is_valid'],
             'is_valid_more' => $oauthInfo['is_valid_more'],
         ];
+
+
+        ## Event
+        #
+        $r = $this->event()
+            ->trigger(EventsHeapOfProfile::RETRIEVE_PROFILE_RESULT, [
+                /** @see Profile\Events\ */
+                'result' => $r, 'userid' => $userid, 'entity_profile' => $entity, 'visitor' => $userid,
+            ])
+            ->then(function ($collector) {
+                /** @var \Module\Profile\Events\DataCollector $collector */
+                return $collector->getResult();
+            });
+
 
         return [
             ListenerDispatch::RESULT_DISPATCH => $r
